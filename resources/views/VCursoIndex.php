582eff2217530449b58dae3f0647f1e600020485
Koppe <mateus.koppe@gmail.com>
2017-03-27 18:50:14 -0300

    Sass implementations

--- resources/views/VCursoIndex.php
+++ resources/views/VCursoIndex.php
@@ -1,1 +1,8 @@
-<h1>Cursos </h1>
+<div class="container">
+	<h1>Cursos </h1>
+	<div class="row">
+		<div class="col-xl-2 col-md-4 col-sm-5 col-xs-6">Test</div>
+		<div class="col-xl-2 col-md-4 col-sm-5 col-xs-6">Test</div>
+		<div class="col" style="background-color: blue">col</div>
+	</div>
+</div>
--- resources/views/structure/header.php
+++ resources/views/structure/header.php
@@ -3,5 +3,7 @@
 <head>
 	<title>SENAI Cursos</title>
 	<meta charset="utf-8">	
+	<link rel="stylesheet" href="./template/dist/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
