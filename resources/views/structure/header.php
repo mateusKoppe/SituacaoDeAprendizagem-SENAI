24f8499ebf8f14648de5775bbd5077771ec04032
Koppe <mateus.koppe@gmail.com>
2017-03-28 21:56:53 -0300

    Header some things, grid update, helpers update

--- resources/views/structure/header.php
+++ resources/views/structure/header.php
@@ -3,7 +3,41 @@
 <head>
 	<title>SENAI Cursos</title>
 	<meta charset="utf-8">	
+	<link rel="icon" href="./template/media/favicon.ico" type="image/x-ico">
 	<link rel="stylesheet" href="./template/dist/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+
+	<header id="main-header">
+		<nav class="institutions-menu">
+			<div class="container">
+				<ul class="institutions-menu__list">
+					<li class="institutions-menu__item">
+						<a class="institutions-menu__link" href="http://fiesc.com.br" target="blank">FIESC</a>
+					</li>
+					<li class="institutions-menu__item">
+						<a class="institutions-menu__link" href="http://ciesc.com.br" target="blank">CIESC</a>
+					</li>
+					<li class="institutions-menu__item">
+						<a class="institutions-menu__link" href="http://sesisc.org.br" target="blank">SESI</a>
+					</li>
+					<li class="institutions-menu__item">
+						<a class="institutions-menu__link" href="http://sc.senai.br" target="blank">SENAI</a>
+					</li>
+					<li class="institutions-menu__item">
+						<a class="institutions-menu__link" href="http://ielsc.org.br" target="blank">IEL</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="main-header-content">
+			<div class="container container--full-height">
+				<div class="row row--full-height">
+					<div class="col-5 col--v-center">
+						<img src="./template/media/logo-with-description.png">
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
