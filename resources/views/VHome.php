0b1efb117afc5daf80d3af025d9db12f3d2b76b9
Koppe <mateus.koppe@gmail.com>
2017-05-11 19:44:44 -0300

    Slider class

--- resources/views/VHome.php
+++ resources/views/VHome.php
@@ -1,7 +1,7 @@
 <main>
 	<div class="banner-slider banner-slider--js">
 		<div class="banner-top-slider banner-slider__wrapper">
-			<div class="banner-slider__slide banner-slider__slide--active"><img src="./template/media/banner1.jpg" width="100%"></div>
+			<div class="banner-slider__slide"><img src="./template/media/banner1.jpg" width="100%"></div>
 			<div class="banner-slider__slide"><img src="./template/media/banner2.jpg" width="100%"></div>
 			<div class="banner-slider__slide"><img src="./template/media/banner3.jpg" width="100%"></div>
 			<div class="banner-slider__slide"><img src="./template/media/banner4.jpg" width="100%"></div>
--- template/scripts/banner-slider.js
+++ template/scripts/banner-slider.js
@@ -1,14 +1,37 @@
 class BannerSlider {
 
 	constructor(banner, config) {
-		this.banner = banner;
-		this.config = config;
+		this._banner = banner;
+		this._config = config;
 
 		this.create();
 	}
 
 	create(){
-		alert(this.banner);
+		this.sliders = this._banner.querySelectorAll(".banner-slider__slide");
+		this.actualIndexSlider = 0;
+		
+
+		setInterval(() => this._nextSlider(), 2000);
+		this._nextSlider();
+	}
+
+	_nextSlider(){
+		if(this._activeSlide) this._activeSlide.classList.remove('banner-slider__slide--active');
+		if(!this.sliders[this.actualIndexSlider]){
+			this.actualIndexSlider = 0;
+		}
+		this._activeSlide = this.sliders[this.actualIndexSlider];
+		this._activeSlide.classList.add('banner-slider__slide--active')
+		this.actualIndexSlider++;
+	}
+
+	_prevSlider(){
+		this.actualIndexSlider--;
+		if(!this.sliders[this.actualIndexSlider]){
+			this.actualIndexSlider = this.sliders.length - 1;
+		}
+		console.log(this.sliders[this.actualIndexSlider]);
 	}
 	
 }
